b505ac905a40bffc02e8500b5f10eae1664de4a6
krishna <ki54.jhapate@gmail.com>
2023-10-25 14:54:13 +0530

    carousal images dynamic

--- src/components/pages/home/Hero.jsx
+++ src/components/pages/home/Hero.jsx
@@ -1,16 +1,19 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import Carousel from 'react-material-ui-carousel'
+import { useSelector } from 'react-redux'
+import { baseUrl } from '../../../utils'
 
 
 function Hero() {
-    const images = ['https://img-b.udemycdn.com/notices/featured_carousel_slide/image/6dbe7b9f-d020-460a-aef3-d33c852e4192.jpg', 'https://img-b.udemycdn.com/notices/featured_carousel_slide/image/acd50903-a00d-41e7-a6d8-8ee6ea729a29.jpg']
+    const { data: carousalImages } = useSelector(state => state.landingPage.data)
+
     return (
         <Box width={'90%'} mx='auto'  >
             <Carousel style={{ width: '70vw', margin: '0 auto' }} >
-                {images.map((image, index) => (
+                {carousalImages.attributes.carousal.map((image, index) => (
                     <div key={index} style={{ margin: '0 auto' }} >
-                        <img src={image} alt={`Image ${index}`} style={{ margin: '0 auto' }} />
+                        <img src={`${baseUrl}${image.image.data.attributes.url}`} alt={`Image ${index}`} style={{ margin: '0 auto' }} />
                     </div>
                 ))}
             </Carousel>
